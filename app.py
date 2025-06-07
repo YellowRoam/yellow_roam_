a21a902cf6a8d7050d84b0dc3d805ab9edb86bae
YellowRoam <heyday6159@gmail.com>
2025-06-06 19:23:48 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,20 @@
-from flask import Flask
+import os
+from flask import Flask, render_template
+
 app = Flask(__name__)
-@app.route("/")@app.route("/")
+
+@app.route("/")
 def home():
-    return "YellowRoam is live!"
+    return render_template("index.html")
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("404.html"), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template("500.html"), 500
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
