d53d9f914600afab0ac1315a33d0456e61fbdf31
YellowRoam <heyday6159@gmail.com>
2025-06-13 20:06:30 -0600

    Update cors_config.py

--- cors_config.py
+++ cors_config.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 
 def configure_cors(app):
     CORS(
-        app,Add commentMore actions
+        app,
         resources={r"/api/*": {"origins": "https://yellowroam.github.io"}},
         supports_credentials=True
     )
