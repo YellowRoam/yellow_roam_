1c7960b901a639159e20a65c7a3f47bb73e5d0ed
YellowRoam <heyday6159@gmail.com>
2025-06-16 14:42:04 -0600

    Delete cors_config.py

--- cors_config.py
+++
@@ -1,9 +0,0 @@
-# cors_config.py
-from flask_cors import CORS
-
-def configure_cors(app):
-    CORS(
-        app,
-        resources={r"/api/*": {"origins": "https://yellowroam.github.io"}},
-        supports_credentials=True
-    )
